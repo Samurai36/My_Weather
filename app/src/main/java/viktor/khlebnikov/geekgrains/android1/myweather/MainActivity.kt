620274871edc4d05575f95058a2c1b5f37b10005
K.Viktor <svry88@gmail.com>
2021-05-13 17:24:56 +0300

    add HW1 (empty project)

---
+++ app/src/main/java/viktor/khlebnikov/geekgrains/android1/myweather/MainActivity.kt
@@ -0,0 +1,11 @@
+package viktor.khlebnikov.geekgrains.android1.myweather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
